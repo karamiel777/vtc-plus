f32ed2988f197b79cf9a65325a2190c661a22b04
karamiel777 <hasymila@gmail.com>
2026-09-17 14:03:38 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "vtc-plus-a6242.firebaseapp.com",
+  projectId: "vtc-plus-a6242",
+  storageBucket: "vtc-plus-a6242.firebasestorage.app",
+  messagingSenderId: "500809490080",
+  appId: "1:500809490080:web:111996fe837bad8bcdb3c5"
+};
+
+const app = initializeApp(firebaseConfig);
